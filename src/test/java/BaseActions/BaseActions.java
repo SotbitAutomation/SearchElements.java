87b2d628e671c429bbf0653477a2504882605d85
Sotbit <info@sotbit.ru>
2022-01-31 10:40:04 +0300

    удалдение комментов для тестов

--- src/test/java/BaseActions/BaseActions.java
+++ src/test/java/BaseActions/BaseActions.java
@@ -1,5 +1,5 @@
 package BaseActions;
-//123131
+
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.openqa.selenium.By;
@@ -13,7 +13,7 @@ import java.io.*;
 import java.security.SecureRandom;
 import java.util.Collection;
 
-//test push
+
 public class BaseActions extends CustomizingForYourself {
 
     public JavascriptExecutor jse = (JavascriptExecutor)driver;
